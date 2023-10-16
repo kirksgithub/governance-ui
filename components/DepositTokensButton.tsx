efa59789ca6a2c4d12207f20e4305bdf3f88376e
Kirk <kfournia@gmail.com>
2023-10-16 20:51:35 +0000

    Removed Disable Button

--- components/DepositTokensButton.tsx
+++ components/DepositTokensButton.tsx
@@ -88,7 +88,6 @@ export const DepositTokensButton = ({
                 await deposit(nativeAmount)
                 setOpenModal(false)
               }}
-              disabled={humanReadableMax !== undefined && (parseInt(amount) > humanReadableMax || parseInt(amount) > 0)}
             >
               Confirm
             </Button>
